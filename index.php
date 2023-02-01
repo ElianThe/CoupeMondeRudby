72f7fcb6ccb28ca295a302bdc76b03978399c811
Guiffault Elian <guiffaul2u@etu.univ-lorraine.fr>
2023-02-01 11:29:12 +0100

    create the models in the namespace \rugby\models

--- index.php
+++ index.php
@@ -3,4 +3,15 @@
 declare(strict_types=1);
 require_once 'vendor/autoload.php';
 
-echo "bonjour";
+use Illuminate\Database\Capsule\Manager as DB;
+use rugby\models\Poste as Poste;
+
+use rugby\models\Equipe as Equipe;
+
+$db = new DB();
+$db->addConnection(parse_ini_file("src/conf/conf.ini"));
+$db->bootEloquent();
+
+
+
+echo "Question 1: </br>";
